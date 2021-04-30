055ea14f58d2c6c14b518f0580227a4be404513e
liorgd <LGdaliahu@perforce.com>
2021-04-30 16:46:14 +0300

    update

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -27,50 +27,6 @@ public class DemoApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
         timeHandler.processData();
-        //timeHandler.printBuckets();
-/*
-        eventsFile.loadFile();
-        List<String> fileLines = eventsFile.getFileLines();
-        System.out.println("All from file:");
-        fileLines.forEach(System.out::println);
-        String currentHandlingHour = null;
-        long numOfSamples = 0;
-        double sumOfTimes = 0;
-        double average = 0;
-        System.out.println(" calculation and results:");
-        for (String line : fileLines) {
-            String[] values = line.split(" ");
-            String nearestHour = timeHandler.getNearestHour(values[0] + " " + values[1]).toString();
-            double dvalue = Double.parseDouble(values[2]);
-            if (currentHandlingHour == null) {
-                currentHandlingHour = nearestHour;
-            }
-            if (!currentHandlingHour.equals(nearestHour)) { // nearestHour has changed.
-                if (numOfSamples != 0) {
-                    // compute
-                    average = sumOfTimes / numOfSamples;
-                }
-                // reset
-                System.out.println("saving: " + currentHandlingHour + " average:" + average);
-                currentHandlingHour = nearestHour;
-                numOfSamples = 0;
-                sumOfTimes = 0;
-
-            } else {
-                sumOfTimes += dvalue;
-                numOfSamples++;
-                System.out.println("adding: " + dvalue + " result:" + sumOfTimes + " currentHandlingHour:" + currentHandlingHour);
-            }
-
-        }
-        if (numOfSamples != 0) {
-            // compute
-            average = sumOfTimes / numOfSamples;
-        }
-
-        System.out.println("saving: " + currentHandlingHour + " average:" + average);
-*/
-
     }
 
 }
